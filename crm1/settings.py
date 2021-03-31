2cb65e8c9695997cd3ea32ff4d2f4da5bbd44c7b
Meet Vaghasia <meet.bda1729@ict.gnu.ac.in>
2021-04-01 02:10:45 +0530

    Error Solving

--- crm1/settings.py
+++ crm1/settings.py
@@ -134,6 +134,6 @@ MEDIA_URL = '/images/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
